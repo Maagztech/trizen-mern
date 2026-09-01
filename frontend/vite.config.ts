77c752feee2fd47fff18586dd62de16cbfb1ca09
Maagztech <pasutosh15@gmail.com>
2026-09-01 15:00:08 +0530

    config changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
+
   server: {
     port: 5173,
     host: true,
+    allowedHosts: ["trizen-service.onrender.com"],
+  },
+
+  preview: {
+    port: 5173,
+    host: true,
+    allowedHosts: ["trizen-service.onrender.com"],
   },
 });
